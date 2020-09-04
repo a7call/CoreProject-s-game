560d4634794613ec7f90903af058a00a7e2e5c7c
a7call <69578951+a7call@users.noreply.github.com>
2020-09-04 19:33:50 +0200

    End Player Mouvement

--- Assets/Scripts/Player/PlayerMouvement.cs
+++ Assets/Scripts/Player/PlayerMouvement.cs
@@ -10,31 +10,80 @@ public class PlayerMouvement : MonoBehaviour
     
     private Vector3 velocity = Vector3.zero;
     public Animator animator;
-    public float smoothTime;
+    public float StartSmoothTime;
+    public float StopSmoothTime;
     private Vector2 mouvementVector;
 
    
     void Update()
     {
-        mouvement.x = Input.GetAxis("Horizontal");
-        mouvement.y = Input.GetAxis("Vertical");
-        mouvementVector = Vector2.ClampMagnitude(mouvement, 1);
+        CheckInputs();
+        GetInputAxis();
+        ClampMouvement(mouvement);
+        GetLastDirection();
+        SetAnimationVariable();
+
 
     }
 
     private void FixedUpdate()
     {
         MovePlayer(mouvementVector * mooveSpeed * Time.fixedDeltaTime);
-        animator.SetFloat("horizontalSpeed", mouvement.x);
-        animator.SetFloat("verticalSpeed", mouvement.y);
-        float playerSpeed = rb.velocity.sqrMagnitude;
+        
+    }
+
+
+
+    //Smooth Player mouvement
+    void MovePlayer(Vector2 _mouvement)
+    {
+        Vector3 targetVelocity = _mouvement;
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, StartSmoothTime);
+    }
+
+
+    // Same Speed when Input (x,y)
+    void ClampMouvement(Vector2 _mouvement)
+    {
+      
+       mouvementVector = Vector2.ClampMagnitude(_mouvement, 1);
+    }
+
+    // Animation Variable
+    void SetAnimationVariable()
+    {
+        animator.SetFloat("HorizontalSpeed", mouvement.x);
+        animator.SetFloat("VerticalSpeed", mouvement.y);
+        float playerSpeed = mouvement.sqrMagnitude;
         animator.SetFloat("Speed", playerSpeed);
     }
 
-    void MovePlayer(Vector2 _mouvement)
+    // Get Input
+    void GetInputAxis()
     {
-        Vector3 targetVelocity = mouvement;
-        rb.velocity = Vector3.SmoothDamp(rb.velocity, _mouvement, ref velocity, smoothTime);
+        mouvement.x = Input.GetAxisRaw("Horizontal");
+        mouvement.y = Input.GetAxisRaw("Vertical");
+       
+    }
+
+    // Get last Direction for Idle
+    void GetLastDirection()
+    {
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == -1 || Input.GetAxisRaw("Vertical") == 1)
+        {
+            animator.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            animator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+
+        }
+    }
+
+    // Check If Player released Inputs
+    void CheckInputs()
+    {
+        if (mouvement == Vector2.zero)
+        {
+            rb.velocity = Vector3.SmoothDamp(rb.velocity, Vector2.zero, ref velocity, StopSmoothTime);
+        }
     }
 
 }
