53c6a3d14afd7e1f3b238a81f67afaeefd22db07
Aeroxal <69767755+Aeroxal@users.noreply.github.com>
2020-09-30 17:55:36 +0200

    Création d'un ScriptableObjects pour les Potions + Fonction d'utilisation des items

---
+++ Assets/Scripts/UsingItems/UsingItems.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Classe de l'inventaire
+/// 
+/// </summary>
+
+public class UsingItems : ItemObjects
+{
+    // Déclarations d'autres variables
+    private PlayerHealth playerHealth;
+    private PlayerMouvement playerMouvement;
+    private PlayerAttack playerAttack;
+
+    // Déclarations des items
+    [SerializeField] protected ItemObjects ItemObjectsData;
+
+    //Méthode SetData()
+    protected virtual void SetData()
+    {
+        id = ItemObjectsData.id;
+        nameObject = ItemObjectsData.nameObject;
+        description = ItemObjectsData.description;
+        Image = ItemObjectsData.Image;
+        coastAtTrader = ItemObjectsData.coastAtTrader;
+        hpGiven = ItemObjectsData.hpGiven;
+        speed = ItemObjectsData.speed;
+        speedDuration = ItemObjectsData.speedDuration;
+        increasedDamages = ItemObjectsData.increasedDamages;
+        attackSpeed = ItemObjectsData.attackSpeed;
+        pointShield = ItemObjectsData.pointShield;
+    }
+
+    private void Start()
+    {
+        playerHealth = FindObjectOfType<PlayerHealth>();
+        playerMouvement = FindObjectOfType<PlayerMouvement>();
+        playerAttack = FindObjectOfType<PlayerAttack>();
+    }
+
+    private void ConsumeHpPotion()
+    {
+        playerHealth.currentHealth += hpGiven;
+    }
+
+    ////Coroutine
+    //private void ConsumeMSPotion()
+    //{
+    //    playerMouvement.speed 
+    //}
+
+    //private void ConsumeASPotion()
+    //{
+
+    //}
+}
