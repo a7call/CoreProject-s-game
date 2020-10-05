c64c26678ffd31d1f4a0999614c554be01c5c45f
a7call <69578951+a7call@users.noreply.github.com>
2020-10-05 16:07:16 +0200

    Add gestion des armes à distance  + switch d'attaque mode

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -9,27 +9,39 @@ public class PlayerAttack : MonoBehaviour
 {
 
     public PlayerScriptableObjectScript playerData;
-    public Transform attackPoint;
     public Animator animator;
     public LayerMask enemyLayer;
     public float attackRadius;
     public GameObject projectil;
+    private GameObject cacWeapons;
+    private bool isCaC = true;
 
+    private void Awake()
+    {
+        cacWeapons = GameObject.FindGameObjectWithTag("WeaponManager");
+    }
     // Update is called once per frame
     void Update()
     {
-       
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            SwitchAttackMode();
+        }
+        if (!isCaC)
+        {
+            GetAttackDirection();
+        }
     }
 
  
 
-    void AttackCACMono()
-    {
-        Collider2D enemyHit = Physics2D.OverlapCircle(attackPoint.position, attackRadius, enemyLayer);
+   // void AttackCACMono()
+    //{
+      //  Collider2D enemyHit = Physics2D.OverlapCircle(attackPoint.position, attackRadius, enemyLayer);
 
         // do something
 
-    }
+   // }
 
     void InstantiateProjectile()
     {
@@ -46,8 +58,40 @@ public class PlayerAttack : MonoBehaviour
     }
 
 
-  
-   
+    private void SwitchAttackMode()
+    {
+        if (isCaC)
+        {
+            cacWeapons.SetActive(false);
+            isCaC = false;
+        }
+        else
+        {
+            cacWeapons.SetActive(true);
+            isCaC = true;
+        }
+        
+    }
+
+    [SerializeField]
+    protected Transform attackPoint;
+    Vector3 screenMousePos;
+    Vector3 screenPlayerPos;
+
+    // recupère en temps réel la position de la souris et associe cette position au point d'attaque du Player
+    protected virtual void GetAttackDirection()
+    {
+
+        // position de la souris sur l'écran 
+        screenMousePos = Input.mousePosition;
+        // position du player en pixel sur l'écran 
+        screenPlayerPos = Camera.main.WorldToScreenPoint(transform.position);
+        // position du point d'attaque 
+        attackPoint.position = new Vector2(transform.position.x + (screenMousePos - screenPlayerPos).normalized.x, transform.position.y + (screenMousePos - screenPlayerPos).normalized.y);
+    }
+
+
+
 
 }
 
---
+++ Assets/WeaponDistancePickUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDistancePickUp : MonoBehaviour
+{
+    private PlayerAttack player;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            player = FindObjectOfType<PlayerAttack>();
+            player.projectil = gameObject;
+            Destroy(gameObject);
+        } 
+    }
+}
