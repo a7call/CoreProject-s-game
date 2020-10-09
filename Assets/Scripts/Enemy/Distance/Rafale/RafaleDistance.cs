521ae9ed7834cf52d0a81c6e6c01d78933a01d9a
TestProjet <TestProjet@users.noreply.github.com>
2020-10-09 22:58:12 +0200

    Une bonne journée un bon ennemi (celui qui tire en rafale)

---
+++ Assets/Scripts/Enemy/Distance/Rafale/RafaleDistance.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Classe héritière de Distance.cs
+/// Elle contient les fonctions de la classe mère
+/// </summary>
+public class RafaleDistance : Distance
+{
+    [SerializeField] protected RafaleDistanceData RafaleDistanceData;
+
+    private float timeIntervale;
+    private int nbTir;
+
+    private int n = 0; //compteur pour le while
+
+    protected override void SetData()
+    {
+        base.SetData();
+        timeIntervale = RafaleDistanceData.timeIntervale;
+        nbTir = RafaleDistanceData.nbTir;
+    }
+    
+
+   
+
+    void Start()
+    {
+        currentState = State.Patrolling;
+        // Set premier targetPoint
+        SetFirstPatrolPoint();
+        // Set data
+        SetData();
+        SetMaxHealth();
+    }
+    protected override void Update()
+    {
+        base.Update();
+        switch (currentState)
+        {
+            case State.Patrolling:
+                // script de patrol
+                Patrol();
+                PlayerInSight();
+                MoveToPath();
+                break;
+            case State.Chasing:
+                // récupération de l'aggro
+                Aggro();
+                isInRange();
+                // suit le path créé et s'arrête pour tirer
+                MoveToPath();
+                break;
+            case State.Attacking:
+                isInRange();
+                // Couroutine gérant les shoots 
+                StartCoroutine("CanShoot");
+                break;
+        }
+
+    }
+
+    
+
+    // Mouvement
+
+    // Override(Enemy.cs) Aggro s'arrete pour tirer et suit le player si plus à distance
+    protected override void Aggro()
+    {
+        targetPoint = target;
+    }
+
+    protected override void PlayerInSight()
+    {
+        base.PlayerInSight();
+    }
+
+    protected override void isInRange()
+    {
+        base.isInRange();
+    }
+
+    // Voir Enemy.cs (héritage)
+    protected override void Patrol()
+    {
+        base.Patrol();
+    }
+
+    // Voir Enemy.cs (héritage)
+    protected override void SetFirstPatrolPoint()
+    {
+        base.SetFirstPatrolPoint();
+    }
+
+
+    // Health
+
+
+    // Voir Enemy.cs (héritage)
+    protected override void SetMaxHealth()
+    {
+        base.SetMaxHealth();
+    }
+
+    // Voir Enemy.cs (héritage)
+    protected override void TakeDamage(int _damage)
+    {
+        base.TakeDamage(_damage);
+
+    }
+
+    // Voir Enemy.cs (héritage)
+    protected override IEnumerator WhiteFlash()
+    {
+        return base.WhiteFlash();
+    }
+
+
+    // Attack
+
+    // Voir Enemy.cs (héritage)
+    protected override IEnumerator CanShoot()
+    {
+        return base.CanShoot();
+    }
+
+    // Voir Enemy.cs (héritage)
+    protected override void ResetAggro()
+    {
+        base.ResetAggro();
+    }
+
+
+    // Voir Enemy.cs (héritage)
+    protected override void Shoot()
+    {
+        StartCoroutine(intervalleTir());
+    }
+
+    protected virtual IEnumerator intervalleTir()
+    {
+        
+        while (n < nbTir)
+        {
+            base.Shoot();
+            yield return new WaitForSeconds(timeIntervale);
+            n++;
+        }
+        n = 0;
+    }
+}
---
+++ Assets/Scripts/Enemy/Distance/Rafale/RafaleDistanceData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new DistanceRafaleEnemy", menuName = "DistanceRafaleEnemy")]
+public class RafaleDistanceData : DistanceScriptableObject
+{
+    public float timeIntervale;
+    public int nbTir;
+}
