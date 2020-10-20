8881b2fc75b757a70eb252b06ee0f4a9fc894b42
TestProjet <TestProjet@users.noreply.github.com>
2020-10-20 21:55:58 +0200

    Snot ennemi

---
+++ Assets/Scripts/Enemy/Distance/HitZone/SnotBubble.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Classe héritière de Distance.cs
+/// Elle contient les fonctions de la classe mère
+/// </summary>
+public class SnotBubble : Distance
+{
+    [SerializeField] protected GameObject HitZoneGO;
+    
+
+    void Start()
+    {
+        currentState = State.Patrolling;
+        // Set premier targetPoint
+        SetFirstPatrolPoint();
+        // Set data
+        SetData();
+        SetMaxHealth();
+    }
+    protected override void Update()
+    {
+        base.Update();
+        switch (currentState)
+        {
+            case State.Patrolling:
+                // script de patrol
+                Patrol();
+                PlayerInSight();
+                MoveToPath();
+                break;
+            case State.Chasing:
+                // récupération de l'aggro
+                Aggro();
+                isInRange();
+                // suit le path créé et s'arrête pour tirer
+                MoveToPath();
+                break;
+            case State.Attacking:
+                isInRange();
+                // Couroutine gérant les shoots 
+                StartCoroutine("CanShoot");
+                break;
+        }
+
+
+    }
+
+    protected override void SetData()
+    {
+        base.SetData();
+    }
+
+    // Mouvement
+
+    // Override(Enemy.cs) Aggro s'arrete pour tirer et suit le player si plus à distance
+    protected override void Aggro()
+    {
+        targetPoint = target;
+        GameObject.Instantiate(HitZoneGO, transform.position, Quaternion.identity);
+
+    }
+
+    protected override void PlayerInSight()
+    {
+        base.PlayerInSight();
+    }
+
+    protected override void isInRange()
+    {
+
+        //base.isInRange();
+    }
+
+    // Voir Enemy.cs (héritage)
+    protected override void Patrol()
+    {
+        base.Patrol();
+    }
+
+    // Voir Enemy.cs (héritage)
+    protected override void SetFirstPatrolPoint()
+    {
+        base.SetFirstPatrolPoint();
+    }
+
+
+    // Health
+
+
+    // Voir Enemy.cs (héritage)
+    protected override void SetMaxHealth()
+    {
+        base.SetMaxHealth();
+    }
+
+
+    // Voir Enemy.cs (héritage)
+    protected override IEnumerator WhiteFlash()
+    {
+        return base.WhiteFlash();
+    }
+
+
+    // Attack
+
+    // Voir Enemy.cs (héritage)
+    protected override IEnumerator CanShoot()
+    {
+        return base.CanShoot();
+    }
+
+    // Voir Enemy.cs (héritage)
+    protected override void ResetAggro()
+    {
+        base.ResetAggro();
+    }
+
+
+    // Voir Enemy.cs (héritage)
+    protected override void Shoot()
+    {
+        //base.Shoot();
+    }
+
+}
