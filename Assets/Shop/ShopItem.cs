716be3a2102794a4be112f6e4092c4b54927ade0
Aeroxal <69767755+Aeroxal@users.noreply.github.com>
2020-10-29 14:42:42 +0100

    ShopUpdate

---
+++ Assets/Shop/ShopItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItem : MonoBehaviour
+{
+    public string nameObject;
+    public Text textName;
+    public Text textPrice;
+    public Image imageObject;
+
+    public ShopItemButton shopItemButton;
+
+    private void Start()
+    {
+        nameObject = shopItemButton.name;
+        textName.text = shopItemButton.itemTextName;
+        textPrice.text = shopItemButton.itemTextPrice;
+        imageObject.sprite = shopItemButton.itemImageObject;
+
+    }
+}
---
+++ Assets/Shop/ShopItemButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "ShopItemButton")]
+public class ShopItemButton : ScriptableObject
+{
+    public string itemName;
+    public string itemTextName;
+    public string itemTextPrice;
+    public Sprite itemImageObject;
+}
