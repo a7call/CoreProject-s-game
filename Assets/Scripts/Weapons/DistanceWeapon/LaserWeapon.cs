ef701bf0f019d274ac168442baa95be34cf7663b
TestProjet <TestProjet@users.noreply.github.com>
2020-11-01 19:43:54 +0100

    Pistolet à eau + Fin laser

--- Assets/Scripts/Weapons/DistanceWeapon/LaserWeapon.cs
+++ Assets/Scripts/Weapons/DistanceWeapon/LaserWeapon.cs
@@ -4,22 +4,62 @@ using UnityEngine;
 
 public class LaserWeapon : DistanceWeapon
 {
-    [SerializeField] float LoadingDelay;
+    //[SerializeField] float LoadingDelay;
+    protected int count;
+    protected bool IsToHot = false;
+    protected bool IsCooling = false;
+   [SerializeField] protected float coolingTime;
+   [SerializeField] protected float coolingDelay;
+   [SerializeField] protected int countMax;
 
+   
+
+    protected override void Update()
+    {
+        base.Update();
+        if (!isAttacking && !IsCooling)
+        {
+            StartCoroutine(CoolDelay());
+        }
+    }
 
 
     protected override IEnumerator Shoot()
     {
-        if (!isAttacking)
+        
+        if (!isAttacking && !IsToHot)
         {
-            yield return new WaitForSeconds(LoadingDelay);
+            //yield return new WaitForSeconds(LoadingDelay);
             isAttacking = true;
             Instantiate(projectile, attackPoint.position, Quaternion.identity);
+            count++;
             yield return new WaitForSeconds(attackDelay);
             isAttacking = false;
+            if (count >= countMax)
+            {
+                IsToHot = true;
+                StartCoroutine(LaserCooling());
+            }
         }
 
     }
 
+    protected IEnumerator LaserCooling()
+    {
+        yield return new WaitForSeconds(coolingTime);
+        IsToHot = false;
+        count = 0;
+    }
 
+    protected IEnumerator CoolDelay()
+    {
+        if (count > 0)
+        {
+            IsCooling = true;
+            yield return new WaitForSeconds(coolingDelay);
+            count--;
+            IsCooling = false;
+        }
+        
+    }
 }
