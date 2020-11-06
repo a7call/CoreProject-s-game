88a3e9b07354f391b5a234d0161918ca7c7bcb76
a7call <69578951+a7call@users.noreply.github.com>
2020-11-06 11:21:55 +0100

    Module D'assitant robotique

---
+++ Assets/Scripts/SpecialObjects/PassiveObject/ModuleAssitanceRobotique/ModuleAssitanceRobotique.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleAssitanceRobotique : PassiveObjects
+{
+    [SerializeField] private GameObject robotsAssitant;
+    void Start()
+    {
+        Instantiate(robotsAssitant, transform.position, Quaternion.identity);
+    }
+
+}
