593e95f537aee3e65febc5c18a1e7575530e0274
Aeroxal <69767755+Aeroxal@users.noreply.github.com>
2020-11-07 15:09:07 +0100

    ShopFini

--- Assets/Shop/ScriptItem/ShopItem.cs
+++ Assets/Shop/ScriptItem/ShopItem.cs
@@ -14,6 +14,8 @@ public class ShopItem : MonoBehaviour
     private Inventory inventory;
     private ShopManager shopManager;
 
+    private Button button;
+
     private void Start()
     {
         nameObject = shopItemButton.name;
@@ -23,6 +25,8 @@ public class ShopItem : MonoBehaviour
 
         inventory = FindObjectOfType<Inventory>();
         shopManager = FindObjectOfType<ShopManager>();
+
+        button = gameObject.GetComponent<Button>();
     }
 
     private void Update()
@@ -32,36 +36,38 @@ public class ShopItem : MonoBehaviour
 
     private void SetButtonIteractible()
     {
-        Button button = gameObject.GetComponent<Button>();
-
-        if (inventory.goldPlayer >= shopItemButton.itemPrice)
-        {
-            button.interactable = true;
-        }
-        else
+        if (gameObject.CompareTag("NothingButton"))
         {
             button.interactable = false;
         }
+        else
+        {
+            if (inventory.goldPlayer >= shopItemButton.itemPrice)
+            {
+                button.interactable = true;
+            }
+            else
+            {
+                button.interactable = false;
+            }
+        }
+        
     }
 
     public void BuyItem()
     {
         if (inventory.goldPlayer >= shopItemButton.itemPrice)
         {
+            // Décompte les sous du joueur
             inventory.goldPlayer -= shopItemButton.itemPrice;
-            print("La pos du button à détruire" + gameObject.transform);
-            // On enlève le button du shop
-            // Voir comment récupérer la position du boutton sur lequel on clique
-            Instantiate(shopManager.oneHp, gameObject.transform);
-            gameObject.SetActive(false);
+            // On enlève l'item du shop. Pour ce faire, on change les caractéristiques du bouton, et on le désactive
+            // Pour l'instant, on change juste le texte. Quand on détaillera le shop, il faudra reprendre ce point
+            GetComponent<ShopItem>().textName.GetComponent<Text>().text = "Nothing";
+            // Désactivation et non intéractabilité possible du bouton
+            GetComponent<ShopItem>().enabled = false;
+            button.interactable = false;
             // On ajoute l'item à l'inventaire
             inventory.itemInventory.Add(gameObject);
-            // On rajouter un boutton nothing
-            Debug.Log("A acheté l'item, donc celui-ci disparait du shop");
-        }
-        else
-        {
-            Debug.Log("N'a pas les sous pour acheter l'item");
         }
     }
 }
