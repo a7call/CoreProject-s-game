f2eed819cc925b74a7a2731707c1a60e992a6b33
TestProjet <TestProjet@users.noreply.github.com>
2021-01-17 17:46:27 +0100

    Coffre armes + Coffre Spawner

---
+++ Assets/Scripts/Rewards/Coffres/CoffreSpawner.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffreSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] protected float chanceToGetCoffre1 = 0.4f;
+    [SerializeField] protected float chanceToGetCoffre2 = 0.3f;
+    [SerializeField] protected float chanceToGetCoffre3 = 0.2f;
+    [SerializeField] protected float chanceToGetCoffre4 = 0.1f;
+    protected float chanceToDrop = 0;
+    public int nbC1 = 0;
+    public int nbC2 = 0;
+    public int nbC3 = 0;
+    public int nbC4 = 0;
+    public int nbC = 0;
+
+    public GameObject Coffre1 = null;
+    public GameObject Coffre2 = null;
+    public GameObject Coffre3 = null;
+    public GameObject Coffre4 = null;
+
+
+    void Start()
+    {
+        DropCoffre();
+    }
+
+    private void DropCoffre()
+    {
+        chanceToDrop = Random.Range(0f, 1f);
+        nbC++;
+        if (chanceToDrop <= chanceToGetCoffre4)
+        {
+            GameObject.Instantiate(Coffre4, transform.position,Quaternion.identity);
+            nbC4++;
+           
+         }
+        else if (chanceToDrop > chanceToGetCoffre4 && chanceToDrop <= chanceToGetCoffre2)
+        {
+            GameObject.Instantiate(Coffre3, transform.position, Quaternion.identity);
+            nbC3++;
+           
+        }
+        else if (chanceToDrop > chanceToGetCoffre3 && chanceToDrop <= 1 - chanceToGetCoffre1)
+        {
+            GameObject.Instantiate(Coffre2, transform.position, Quaternion.identity);
+            nbC2++;
+            
+        }
+        else if (chanceToDrop > 1 - chanceToGetCoffre1)
+        {
+            GameObject.Instantiate(Coffre1, transform.position, Quaternion.identity);
+            nbC1++;
+            
+        }
+
+    }
+
+    
+   
+}
