59ad2ae6049ec4d241e6393ada01fe91f3400a0d
a7call <69578951+a7call@users.noreply.github.com>
2021-01-26 13:46:20 +0100

    Add Edgar

---
+++ Assets/Edgar/Runtime/FogOfWar/FogOfWarAdditionalCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Edgar.Unity
+{
+    public class FogOfWarAdditionalCamera : MonoBehaviour
+    {
+        private void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            FogOfWar.Instance?.OnRenderImage(source, destination, GetComponent<Camera>());
+        }
+    }
+}
