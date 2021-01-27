c4037c3ac8a72263a5f17a067952e04fc1420f27
a7call <69578951+a7call@users.noreply.github.com>
2021-01-27 16:03:31 +0100

    TileSheet

---
+++ Assets/Edgar/MyEdgarImplementation/Rooms/RoomType.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RoomType 
+{
+    Boss,
+    Normal,
+    Hub,
+    Spawn,
+    Shop,
+    Reward
+}
---
+++ Assets/Edgar/MyEdgarImplementation/Rooms/WandererRoom.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Edgar.Unity;
+
+
+public class WandererRoom : RoomBase
+{
+    public RoomType Type;
+  
+
+
+    public override List<GameObject> GetRoomTemplates()
+    {
+        // We do not need any room templates here because they are resolved based on the type of the room.
+        return null;
+    }
+
+    public override string GetDisplayName()
+    {
+        // Use the type of the room as its display name.
+        return Type.ToString();
+    }
+
+    public override RoomEditorStyle GetEditorStyle(bool isFocused)
+    {
+        var style = base.GetEditorStyle(isFocused);
+
+        var backgroundColor = style.BackgroundColor;
+
+        // Use different colors for different types of rooms
+        switch (Type)
+        {
+            case RoomType.Spawn:
+                backgroundColor = new Color(38 / 256f, 115 / 256f, 38 / 256f);
+                break;
+
+            case RoomType.Boss:
+                backgroundColor = new Color(128 / 256f, 0 / 256f, 0 / 256f);
+                break;
+
+            case RoomType.Shop:
+                backgroundColor = new Color(102 / 256f, 0 / 256f, 204 / 256f);
+                break;
+
+            case RoomType.Reward:
+                backgroundColor = new Color(102 / 256f, 0 / 256f, 204 / 256f);
+                break;
+        }
+
+        style.BackgroundColor = backgroundColor;
+
+        // Darken the color when focused
+        if (isFocused)
+        {
+            style.BackgroundColor = Color.Lerp(style.BackgroundColor, Color.black, 0.7f);
+        }
+
+        return style;
+    }
+}
