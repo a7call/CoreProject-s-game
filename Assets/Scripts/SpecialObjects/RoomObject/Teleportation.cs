f33264fd9711c4151627b8642806af9ccb82867f
Aeroxal <69767755+Aeroxal@users.noreply.github.com>
2021-02-01 21:21:42 +0100

    TP

---
+++ Assets/Scripts/SpecialObjects/RoomObject/Teleportation.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Teleportation : MonoBehaviour
+{
+    private Player player;
+    public GameObject portal;
+
+    private float reloadDelay = 3f;
+    [SerializeField] private static bool canTp = true;
+
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    private void Update()
+    {
+        print(canTp);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") && canTp)
+        {
+            StartCoroutine(Teleporte());
+        }
+    }
+
+    private IEnumerator Teleporte()
+    {
+        Teleportation.canTp = false;
+        player.transform.position = portal.transform.position;
+        yield return new WaitForSeconds(reloadDelay);
+        Teleportation.canTp = true;
+    }
+}
