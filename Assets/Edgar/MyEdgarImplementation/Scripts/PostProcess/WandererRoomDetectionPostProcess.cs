e47bcab7eb2b466b31bb010814bad65ce2b9e72b
a7call <69578951+a7call@users.noreply.github.com>
2021-02-01 12:23:21 +0100

    Du Donjon

--- Assets/Edgar/MyEdgarImplementation/Scripts/PostProcess/WandererRoomDetectionPostProcess.cs
+++ Assets/Edgar/MyEdgarImplementation/Scripts/PostProcess/WandererRoomDetectionPostProcess.cs
@@ -8,12 +8,15 @@ namespace Edgar.Unity.Examples
     [CreateAssetMenu(menuName = "Edgar/Wanderer/Current room detection/Post-process", fileName = "CurrentRoomDetectionPostProcess")]
     public class WandererRoomDetectionPostProcess : DungeonGeneratorPostProcessBase
     {
+
+        public GameObject[] Enemies;
         public override void Run(GeneratedLevel level, LevelDescription levelDescription)
         {
             foreach (var roomInstance in level.GetRoomInstances())
             {
                 var roomTemplateInstance = roomInstance.RoomTemplateInstance;
                 // Find floor tilemap layer
+                var room = (WandererRoom)roomInstance.Room;
                 var tilemaps = RoomTemplateUtils.GetTilemaps(roomTemplateInstance);
                 var floor = tilemaps.Single(x => x.name == "Floor").gameObject;
                 // Add floor collider
@@ -25,6 +28,57 @@ namespace Edgar.Unity.Examples
                 AddFloorCollider(floor);
                 // Add current room detection handler
                 floor.AddComponent<WandererCurrentRoomDetectionTriggerhandler>();
+
+                if (WandererGameManager.Instance != null)
+                {
+                    // Set the Random instance of the GameManager to be the same instance as we use in the generator
+                    WandererGameManager.Instance.Random = Random;
+                }
+
+                if (room.Type != RoomType.Corridor)
+                {
+                    // Set enemies and floor collider to the room manager
+                    roomManager.Enemies = Enemies;
+                    roomManager.FloorCollider = floor.GetComponent<CompositeCollider2D>();
+
+                    // Find all the doors of neighboring corridors and save them in the room manager
+                    // The term "door" has two different meanings here:
+                    //   1. it represents the connection point between two rooms in the level
+                    //   2. it represents the door game object that we have inside each corridor
+                    foreach (var door in roomInstance.Doors)
+                    {
+                        // Get the room instance of the room that is connected via this door
+                        var corridorRoom = door.ConnectedRoomInstance;
+
+                        // Get the room template instance of the corridor room
+                        var corridorGameObject = corridorRoom.RoomTemplateInstance;
+
+                        // Find the door game object by its name
+                        var doorsGameObject = corridorGameObject.transform.Find("Door")?.gameObject;
+
+                        // Each corridor room instance has a connection that represents the edge in the level graph
+                        // We use the connection object to check if the corridor should be locked or not
+                        var connection = (WandererConnection)corridorRoom.Connection;
+
+                        if (doorsGameObject != null)
+                        {
+                            // If the connection is locked, we set the Locked state and keep the game object active
+                            // Otherwise we set the EnemyLocked state and deactivate the door. That means that the door is active and locked
+                            // only when there are enemies in the room.
+                            if (connection.IsLocked)
+                            {
+                                doorsGameObject.GetComponent<WandererDoors>().State = WandererDoors.DoorState.Locked;
+                            }
+                            else
+                            {
+                                doorsGameObject.GetComponent<WandererDoors>().State = WandererDoors.DoorState.EnemyLocked;
+                                doorsGameObject.SetActive(false);
+                            }
+
+                            roomManager.Doors.Add(doorsGameObject);
+                        }
+                    }
+                }
             }
         }
         protected void AddFloorCollider(GameObject floor)
