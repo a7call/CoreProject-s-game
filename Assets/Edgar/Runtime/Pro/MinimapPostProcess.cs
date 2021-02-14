a98333632209f1a94850b45fc0b270cf5c890940
a7call <69578951+a7call@users.noreply.github.com>
2021-02-14 11:32:21 +0100

    minimap

--- Assets/Edgar/Runtime/Pro/MinimapPostProcess.cs
+++ Assets/Edgar/Runtime/Pro/MinimapPostProcess.cs
@@ -87,10 +87,11 @@ namespace Edgar.Unity
                 // Go through positions inside the bounds of the tilemap
                 foreach (var tilemapPosition in sourceTilemap.cellBounds.allPositionsWithin)
                 {
+                    
                     // Check if there is a tile at a given position
                     var originalTile = sourceTilemap.GetTile(tilemapPosition);
 
-                    if (originalTile != null)
+                    if (originalTile != null )
                     {
                         // If a tile filter is provided, use it to check if the predicate holds
                         if (tileFilter != null)
