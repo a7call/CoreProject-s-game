fdaaa9638830d8fcf3e9ab46778c4b19f87e7c69
a7call <69578951+a7call@users.noreply.github.com>
2021-04-28 22:18:28 +0200

    Electrique Sword

--- Assets/Scripts/Weapons/CacWeapons/Armes Spe/ElectrikSword/ElectrikSword.cs
+++ Assets/Scripts/Weapons/CacWeapons/Armes Spe/ElectrikSword/ElectrikSword.cs
@@ -6,35 +6,35 @@ public class ElectrikSword : CacWeapons
 {
     [SerializeField] protected float electrificationRadius;
 
-    protected override IEnumerator Attack()
-    {
+    //protected override IEnumerator Attack()
+    //{
 
-        if (!isAttacking)
-        {
-            isAttacking = true;
-            Collider2D[] enemyHit = Physics2D.OverlapCircleAll(attackPoint.position, attackRadius, enemyLayer);
+    //    if (!isAttacking)
+    //    {
+    //        isAttacking = true;
+    //        Collider2D[] enemyHit = Physics2D.OverlapCircleAll(attackPoint.position, attackRadius, enemyLayer);
 
-            AttackAppliedOnEnemy(enemyHit);
-            foreach (Collider2D enemy in enemyHit)
-            {
-                DamageToNearEnemyElectrified(enemy);
-            }
-            yield return new WaitForSeconds(attackDelay);
-            isAttacking = false;
-        }
-    }
-    private void DamageToNearEnemyElectrified(Collider2D enemy)
-    {
-
-
-        Collider2D[] enemyElectrified = Physics2D.OverlapCircleAll(enemy.transform.position, electrificationRadius, enemyLayer);
+    //        AttackAppliedOnEnemy(enemyHit);
+    //        foreach (Collider2D enemy in enemyHit)
+    //        {
+    //            DamageToNearEnemyElectrified(enemy);
+    //        }
+    //        yield return new WaitForSeconds(attackDelay);
+    //        isAttacking = false;
+    //    }
+    //}
+    //private void DamageToNearEnemyElectrified(Collider2D enemy)
+    //{
 
 
-        foreach (Collider2D enemyE in enemyElectrified)
-        {
-            if (enemyE == enemy) continue;
-            Enemy enemyH = enemyE.GetComponent<Enemy>();
-            enemyH.TakeDamage(damage);
-        }
-    }
+    //    Collider2D[] enemyElectrified = Physics2D.OverlapCircleAll(enemy.transform.position, electrificationRadius, enemyLayer);
+
+
+    //    foreach (Collider2D enemyE in enemyElectrified)
+    //    {
+    //        if (enemyE == enemy) continue;
+    //        Enemy enemyH = enemyE.GetComponent<Enemy>();
+    //        enemyH.TakeDamage(damage);
+    //    }
+    //}
 }
