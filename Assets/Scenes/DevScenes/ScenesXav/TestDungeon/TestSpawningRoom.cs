e155b1a04d12ecf0281319635a0579c4383a80c8
Xavier <dave.xavier30@gmail.com>
2021-09-30 15:39:10 +0200

    <Feature>: Management Wave + Spawn done
    HomemadeDungeon work : on peut faire des vagues et gérer les spawns

--- Assets/Scenes/DevScenes/ScenesXav/TestDungeon/TestSpawningRoom.cs
+++ Assets/Scenes/DevScenes/ScenesXav/TestDungeon/TestSpawningRoom.cs
@@ -1,23 +1,48 @@
 using UnityEngine;
+using System.Collections;
 using System.Linq;
+using Wanderer.Utils;
+using System.Collections.Generic;
 
 
 public class TestSpawningRoom : MonoBehaviour
 {
+
+    #region Variables 
+
     [SerializeField] private GameObject firstWave;
     [SerializeField] private GameObject secondWave;
-    private Enemy[] enemiesFirstWave;
+    [SerializeField] private float[] spawnTimer;
+
+    private List<List<GameObject>> spawnListByWave = new List<List<GameObject>>(); // Liste qui récupère tous les spawns d'ennemis
+    private List<Enemy> enemiesFirstWave = new List<Enemy>();
+   
+    #endregion
+
+    #region Monobehiavour
 
     private void Awake()
     {
-        enemiesFirstWave = (from directChild in firstWave.GetComponentsInChildren<Enemy>()
-                                           where directChild.transform.parent == firstWave.transform
-                                           select directChild).ToArray();
+        spawnListByWave.Add(Utils.FindGameObjectsInChildWithTag(firstWave, "Spawner"));
+        spawnListByWave.Add(Utils.FindGameObjectsInChildWithTag(secondWave, "Spawner"));
+    }
+
+    private void Start()
+    {
+        FindEnemiesInWave(0);
     }
 
     private void Update()
     {
-        if (canSecondWave()) secondWave.SetActive(true);
+        if (canLaunchSecondWave())
+        {
+            secondWave.SetActive(true);
+
+            for (int i = 0; i < spawnListByWave[1].Count; i++)
+            {
+                StartCoroutine(SetActiveSpawn(spawnTimer[i], spawnListByWave[1][i]));
+            }
+        } 
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -25,10 +50,38 @@ public class TestSpawningRoom : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             firstWave.SetActive(true);
+
+            for (int i = 0; i < spawnListByWave[0].Count; i++)
+            {
+                StartCoroutine(SetActiveSpawn(spawnTimer[i], spawnListByWave[0][i]));
+            }
         }
     }
 
-    private bool canSecondWave()
+    #endregion
+
+    private IEnumerator SetActiveSpawn(float _timer, GameObject _GO)
+    {
+        yield return new WaitForSeconds(_timer);
+        _GO.SetActive(true);
+    }
+
+    private void FindEnemiesInWave(int _indexWave)
+    {
+        for (int i = 0; i < spawnListByWave[_indexWave].Count; i++)
+        {
+            Enemy[] enemiesArray = (from directChild in spawnListByWave[_indexWave][i].GetComponentsInChildren<Enemy>()
+                                    where directChild.transform.parent == spawnListByWave[_indexWave][i].transform
+                                    select directChild).ToArray();
+
+            for (int j = 0; j < enemiesArray.Length; j++)
+            {
+                enemiesFirstWave.Add(enemiesArray[j]);
+            }
+        }
+    }
+
+    private bool canLaunchSecondWave()
     {
         foreach (var enemy in enemiesFirstWave)
         {
@@ -39,5 +92,5 @@ public class TestSpawningRoom : MonoBehaviour
         }
         return true;
     }
-    
+
 }
